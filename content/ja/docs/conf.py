934ca220f2767f78aff93ecf26c4ea065b9ca559
ousttrue <ousttrue@gmail.com>
2022-01-06 19:25:11 +0900

    create po

--- content/ja/docs/conf.py
+++ content/ja/docs/conf.py
@@ -58,3 +58,7 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# sphinx-intl
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_compact = False     # optional.
