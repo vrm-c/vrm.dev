8fec00feac1d6d39e736b317d10611d450d5c8a1
ousttrue <ousttrue@gmail.com>
2024-01-18 13:38:21 +0900

    fix site meta.
    markdownlint-cli2 導入。改行コード

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,51 +1,51 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'VRM',
-  tagline: 'VR向け3Dアバターファイルフォーマット',
-  favicon: 'favicon.ico',
+  title: "VRM",
+  tagline: "VR向け3Dアバターファイルフォーマット",
+  favicon: "favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://vrm.dev',
+  url: "https://vrm.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'VRM Consortium', // Usually your GitHub org/user name.
-  projectName: 'VRM', // Usually your repo name.
+  organizationName: "VRM Consortium", // Usually your GitHub org/user name.
+  projectName: "VRM", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'ja',
-    locales: ['ja'],
+    defaultLocale: "ja",
+    locales: ["ja"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          routeBasePath: '/',
+          sidebarPath: "./sidebars.ts",
+          routeBasePath: "/",
         },
         blog: {
           showReadingTime: false,
-          routeBasePath: '/blog',
-          path: './blog',
-          blogSidebarTitle: 'All posts',
-          blogSidebarCount: 'ALL',
+          routeBasePath: "/blog",
+          path: "./blog",
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -53,140 +53,140 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "images/vrm_topheader.png",
     navbar: {
-      title: '',
+      title: "",
       logo: {
-        alt: 'Vrm Logo',
-        src: 'images/vrm/card.png',
+        alt: "Vrm Logo",
+        src: "images/vrm/card.png",
       },
       items: [
         {
-          label: 'VRM',
-          type: 'docSidebar',
-          sidebarId: 'vrmSidebar',
-          position: 'left',
+          label: "VRM",
+          type: "docSidebar",
+          sidebarId: "vrmSidebar",
+          position: "left",
         },
-        { to: '/showcase', label: 'ShowCase', position: 'left' },
+        { to: "/showcase", label: "ShowCase", position: "left" },
         {
-          label: 'VRM-1.0',
-          type: 'docSidebar',
-          sidebarId: 'vrm1Sidebar',
-          position: 'left',
-        },
-        {
-          label: 'UniVRM-1.0',
-          type: 'docSidebar',
-          sidebarId: 'univrm1Sidebar',
-          position: 'left',
+          label: "VRM-1.0",
+          type: "docSidebar",
+          sidebarId: "vrm1Sidebar",
+          position: "left",
         },
         {
-          label: 'UniVRM',
-          type: 'docSidebar',
-          sidebarId: 'univrmSidebar',
-          position: 'left',
+          label: "UniVRM-1.0",
+          type: "docSidebar",
+          sidebarId: "univrm1Sidebar",
+          position: "left",
         },
         {
-          label: 'glTF',
-          type: 'docSidebar',
-          sidebarId: 'gltfSidebar',
-          position: 'left',
+          label: "UniVRM",
+          type: "docSidebar",
+          sidebarId: "univrmSidebar",
+          position: "left",
         },
-        { to: '/tags', label: 'Tags', position: 'left' },
-        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          label: "glTF",
+          type: "docSidebar",
+          sidebarId: "gltfSidebar",
+          position: "left",
+        },
+        { to: "/tags", label: "Tags", position: "left" },
+        { to: "/blog", label: "Blog", position: "left" },
         //
         {
-          label: 'Site',
-          type: 'docSidebar',
-          sidebarId: 'siteSidebar',
-          position: 'right',
+          label: "Site",
+          type: "docSidebar",
+          sidebarId: "siteSidebar",
+          position: "right",
         },
         {
-          label: 'UniHumanoid',
-          type: 'docSidebar',
-          sidebarId: 'unihumanoidSidebar',
-          position: 'right',
+          label: "UniHumanoid",
+          type: "docSidebar",
+          sidebarId: "unihumanoidSidebar",
+          position: "right",
         },
         {
-          label: 'API',
-          type: 'docSidebar',
-          sidebarId: 'apiSidebar',
-          position: 'right',
+          label: "API",
+          type: "docSidebar",
+          sidebarId: "apiSidebar",
+          position: "right",
         },
 
         {
-          label: 'ReleaseNote',
-          type: 'docSidebar',
-          sidebarId: 'releaseSidebar',
-          position: 'right',
+          label: "ReleaseNote",
+          type: "docSidebar",
+          sidebarId: "releaseSidebar",
+          position: "right",
         },
         {
-          type: 'localeDropdown',
-          position: 'right',
+          type: "localeDropdown",
+          position: "right",
         },
         {
-          href: 'https://github.com/vrm-c/vrm.dev',
-          position: 'right',
+          href: "https://github.com/vrm-c/vrm.dev",
+          position: "right",
           className: "github-link icon-black",
           "aria-label": "GitHub repository",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'VRM Consortium',
+          title: "VRM Consortium",
           items: [
             {
-              label: 'VRM Consortium',
-              href: 'https://vrm-consortium.org',
+              label: "VRM Consortium",
+              href: "https://vrm-consortium.org",
             },
             {
-              label: 'vrm.dev(This site)',
-              href: 'https://github.com/vrm-c/vrm.dev',
+              label: "vrm.dev(This site)",
+              href: "https://github.com/vrm-c/vrm.dev",
               className: "footer__link-item github-link icon-white",
               "aria-label": "GitHub repository",
             },
             {
-              label: 'vrm.dev.en (English translation)',
-              href: 'https://github.com/vrm-c/vrm.dev.en',
+              label: "vrm.dev.en (English translation)",
+              href: "https://github.com/vrm-c/vrm.dev.en",
               className: "footer__link-item github-link icon-white",
               "aria-label": "GitHub repository",
             },
             {
-              label: 'vrm-specification',
-              href: 'https://github.com/vrm-c/vrm-specification',
+              label: "vrm-specification",
+              href: "https://github.com/vrm-c/vrm-specification",
               className: "footer__link-item github-link icon-white",
               "aria-label": "GitHub repository",
-            },
-          ]
-        },
-        {
-          title: 'UniVRM',
-          items: [
-            {
-              label: 'UniVRM',
-              href: 'https://github.com/vrm-c/UniVRM',
-              className: "footer__link-item github-link icon-white",
-              "aria-label": "GitHub repository",
-            },
-            {
-              label: 'ReleaseNote',
-              to: '/release',
             },
           ],
         },
         {
-          title: 'More',
+          title: "UniVRM",
           items: [
             {
-              label: 'glTF',
-              href: 'https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html',
+              label: "UniVRM",
+              href: "https://github.com/vrm-c/UniVRM",
+              className: "footer__link-item github-link icon-white",
+              "aria-label": "GitHub repository",
             },
             {
-              label: 'Docusaurus',
-              href: 'https://docusaurus.io',
+              label: "ReleaseNote",
+              to: "/release",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "glTF",
+              href: "https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html",
+            },
+            {
+              label: "Docusaurus",
+              href: "https://docusaurus.io",
             },
           ],
         },
@@ -196,18 +196,18 @@ const config: Config = {
     prism: {
       theme: prismThemes.duotoneLight,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ['csharp'],
+      additionalLanguages: ["csharp"],
     },
   } satisfies Preset.ThemeConfig,
 
   plugins: [
     [
-      '@docusaurus/plugin-client-redirects',
+      "@docusaurus/plugin-client-redirects",
       {
         redirects: [
           {
-            to: '/showcase',
-            from: '/vrm/vrm_applications/',
+            to: "/showcase",
+            from: "/vrm/vrm_applications/",
           },
         ],
       },
