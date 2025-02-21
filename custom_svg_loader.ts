076867f1175e02602f7d09a8fba6cdb2706bf7fe
ousttrue <ousttrue@gmail.com>
2025-02-21 23:17:08 +0900

    toString

--- custom_svg_loader.ts
+++ custom_svg_loader.ts
@@ -6,10 +6,9 @@ export default function loader(source) {
   console.log('from-loader', this.context);
 
   source = source.replace(/"([^"]+?\.(png|jpg))"/, (all, unquoted, ext) => {
-    // const img_path = path.join(this.context, unquoted);
-    // const img_content = fs.readFileSync(img_path);
-    // return `"data:image/png;base64,${img_content.toString('base64')}"`;
-    return all;
+    const img_path = path.join(this.context, unquoted);
+    const img_content = fs.readFileSync(img_path);
+    return `"data:image/png;base64,${img_content.toString('base64')}"`;
   });
 
   return source;
